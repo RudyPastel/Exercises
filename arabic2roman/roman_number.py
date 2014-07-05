4a46b9665e8027c534b301eef7042ad91fbae5a6
RudyPastel <rudypastel@iMac-de-Rudy.local>
2014-07-06 00:15:36 +0200

    Fused the two intermediary functions into one and respected the PEP8

--- arabic2roman/roman_number.py
+++ arabic2roman/roman_number.py
@@ -1,50 +1,31 @@
 #Roman characters for units
-units = ['', 'I', 'II', 'III', 'IV','V','VI','VII','VIII','IX']
+units = ['', 'I', 'II', 'III', 'IV', 'V','VI','VII','VIII','IX']
 
 #Roman characters for tens
 tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
 
 #Roman characters for hundreds
-hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC','CM']
+hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
 
 #Roman characters for thousands
 thousands = ['', 'M', 'MM', 'MMM']
 
 #List of the small number conversion table
-smallNumberTable = [units, tens, hundreds, thousands]
+conversion_table = [units, tens, hundreds, thousands]
 
 
-#Conversion for number less than or equal to 3999
-def romanNumberSmall(arabic_number, table=smallNumberTable):
+def arabic2roman(arabic_number, table=conversion_table, sep='-'):
+    #For small number the 'M' convention is used. For bigger number the '-' convention is used.
+    if arabic_number <= 3999:
+        number_list = 4
+    else:
+        number_list = 3
+
     #List of the arabic equivalent of each digit in arabic_number
     roman_number = []
 
     #Counts the current power of ten represented by the last digit
-    powerOf10 = 0
-
-    #arabic_number is integer divided by ten at each steps until it equals zero and we step out of the loop
-    while arabic_number:
-        #Extract the number of units
-        digit = arabic_number % 10
-
-        #Add the new digit conversion to the list as the new first element
-        roman_number[:0] = [table[powerOf10][digit]]
-
-        #Interger divide by ten to pop the digits of units
-        arabic_number /= 10
-
-        #Counts the current power of ten represented by the last digit
-        powerOf10 += 1
-    return ''.join(roman_number)
-
-
-#Conversion for numbers greater than 3999
-def romanNumberBig(arabic_number, table=smallNumberTable, sep='-'):
-    #List of the arabic equivalent of each digit in arabic_number
-    roman_number = []
-
-    #Counts the current power of ten represented by the last digit
-    powerOf10 = 0
+    power_of_10 = 0
 
     #arabic_number is integer divided by ten at each steps until it equals zero and we step out of the loop
     while arabic_number:
@@ -52,19 +33,15 @@ def romanNumberBig(arabic_number, table=smallNumberTable, sep='-'):
         digit = arabic_number % 10
 
         #Add a separation mark for each power of 10**3
-        if not(powerOf10 % 3):
+        if not(power_of_10 % number_list):
             roman_number[:0] = [sep]
         #Add the new digit conversion to the list as the new first element
-        roman_number[:0] = [table[powerOf10 % 3][digit]]
+        roman_number[:0] = [table[power_of_10 % number_list][digit]]
 
         #Interger divide by ten to pop the digits of units
         arabic_number /= 10
 
         #Counts the current power of ten represented by the last digit
-        powerOf10 += 1
-    return ''.join(roman_number[:-1])
+        power_of_10 += 1
 
-def arabic2roman(arabic_number):
-    if arabic_number <= 3999:
-        return romanNumberSmall(arabic_number)
-    return romanNumberBig(arabic_number)
+    return ''.join(roman_number[:-1])
