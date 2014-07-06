9a6cbfbb2f1c05173f0db660d875449ee752e3b8
RudyPastel <rudypastel@iMac-de-Rudy.local>
2014-07-06 12:43:43 +0200

    Simplified comments in the code and corrected the README.md file

--- arabic2roman/README.md
+++ arabic2roman/README.md
@@ -3,7 +3,7 @@
 The function arabic2roman returns a Roman numeral string representation of a given Arabic number. Specifically, it takes a non negative integer in arabic and base 10 format as input and returns its Roman numeral string representation.
 
 ## Roman numeral string representation convention
-Because Roman numeral string representation is not unique, conventions were chosen.
+Among the various existing conventions, the following were chosen.
 - If the input is less than 3999, the 'M' standard convention from wikipedia is used.
     - The symbols I, X, C, and M can be repeated three times in succession, but no more. (They may appear more than three times if they appear non-sequentially, such as XXXIX.) D, L, and V can never be repeated.
     - I can be subtracted from V and X only. X can be subtracted from L and C only. C can be subtracted from D and M only. V, L, and D can never be subtracted
@@ -12,7 +12,7 @@ Because Roman numeral string representation is not unique, conventions were chos
 - If the input is greater or equal to 4000, the following '-' convention is used.
     - Units, tens and hundreds are grouped togather for each power of 1000.
     - A dash '-' represents a power of 1000.
-    - Exemple : 656 980 = DCLVI-CMLXXX. 
+    - Exemple : 656 980 = DCLVI-CMLXXX and 245 000 500 963 = CCXLV- -D-CMLXIII. 
     
 ## Usage
 
--- arabic2roman/roman_number.py
+++ arabic2roman/roman_number.py
@@ -10,38 +10,39 @@ hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
 #Roman characters for thousands
 thousands = ['', 'M', 'MM', 'MMM']
 
-#List of the small number conversion table
+#Conversion table
 conversion_table = [units, tens, hundreds, thousands]
 
 
 def arabic2roman(arabic_number, table=conversion_table, sep='-'):
-    #For small number the 'M' convention is used. For bigger number the '-' convention is used.
+
+    #Roman representation convention
     if arabic_number <= 3999:
         number_list = 4
     else:
         number_list = 3
 
-    #List of the arabic equivalent of each digit in arabic_number
+    #Roman equivalent digit per digit
     roman_number = []
 
-    #Counts the current power of ten represented by the last digit
+    #Power of ten represented by the last digit
     power_of_10 = 0
 
-    #arabic_number is integer divided by ten at each steps until it equals zero and we step out of the loop
+    #arabic_number is converted digit by digit from right to left
     while arabic_number:
-        #Extract the number of units
+
+        #Extract the digits of units
         digit = arabic_number % 10
 
         #Add a separation mark for each power of 10**3
         if not(power_of_10 % number_list):
             roman_number[:0] = [sep]
-        #Add the new digit conversion to the list as the new first element
+
+        #Add new digit
         roman_number[:0] = [table[power_of_10 % number_list][digit]]
 
-        #Interger divide by ten to pop the digits of units
+        #Pop the digits of units
         arabic_number /= 10
-
-        #Counts the current power of ten represented by the last digit
         power_of_10 += 1
 
     return ''.join(roman_number[:-1])
